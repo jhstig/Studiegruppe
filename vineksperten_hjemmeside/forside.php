e8e82de6595b4c8ddc74f006d320ebb8cbaa32b2
jhstig <73479559+jhstig@users.noreply.github.com>
2020-11-05 13:21:18 +0100

    dropdown

---
+++ vineksperten_hjemmeside/forside.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vineksperten</title>
+</head>
+<body>
+    <header>
+        <div id="top-row">
+            <img src="https://picsum.photos/200/100" id="logo">    <!-- billede -->
+            <input type="text" id="search" name="search" value="Søg"><!-- Søgefeldt -->
+            <button>Indkøbskurv</button><!-- Indkøbskurv -->
+        </div>
+        <hr>
+        <div id="secTop-row">
+            <!-- Menu -->
+            <div class="dropdown">
+                <button class="dropbtn" id="rodvin">Rødvin</button>
+                <div class="dropdown-content">
+                    <a href="#">Amerikansk</a>
+                    <a href="#">Italiensk</a>
+                    <a href="#">Fransk</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn" id="hvidvin-menu">Hvidvin</button>
+                <div class="dropdown-content">
+                    <a href="#">Amerikansk</a>
+                    <a href="#">Italiensk</a>
+                    <a href="#">Fransk</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn" id="Rosevin-menu">Rosévin</button>
+                <div class="dropdown-content">
+                    <a href="#">Amerikansk</a>
+                    <a href="#">Italiensk</a>
+                    <a href="#">Fransk</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn" id="spiritus-menu">Spiritus</button>
+                <div class="dropdown-content">
+                    <a href="#">Amerikansk</a>
+                    <a href="#">Italiensk</a>
+                    <a href="#">Fransk</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn" id="delikatesser-menu">Delikatesser</button>
+                <div class="dropdown-content">
+                    <a href="#">Amerikansk</a>
+                    <a href="#">Italiensk</a>
+                    <a href="#">Fransk</a>
+                </div>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
