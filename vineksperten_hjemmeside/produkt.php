451ac999ef8f26c4cd21941c72ff369c816589f4
vemme123 <73479449+vemme123@users.noreply.github.com>
2020-11-16 13:04:44 +0100

    css

--- vineksperten_hjemmeside/produkt.php
+++ vineksperten_hjemmeside/produkt.php
@@ -7,35 +7,38 @@ $product = getJson($_GET['cat'])[$_GET['pid']];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><!-- produktnavn variables --></title>
+    <title>Vineksperten</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
-<header>
-    <div id="top-row">
-        <img src="https://picsum.photos/200/100" id="logo">    <!-- billede -->
-        <input type="text" id="search" name="search" value="Søg"><!-- Søgefeldt -->
-        <button>Indkøbskurv</button><!-- Indkøbskurv -->
-    </div>
-    <hr>
-    <div id="secTop-row">
-        <!-- Menu -->
-        <div class="dropdown">
-            <p id="rodvin">Rødvin</p>
-        </div>
-        <div class="dropdown">
-            <p id="hvidvin-menu">Hvidvin</p>
-        </div>
-        <div class="dropdown">
-            <p id="Rosevin-menu">Rosévin</p>
-        </div>
-        <div class="dropdown">
-            <p id="spiritus-menu">Spiritus</p>
-        </div>
-        <div class="dropdown">
-            <p id="delikatesser-menu">Delikatesser</p>
-        </div>
-    </div>
-</header>
 <body>
+  <header>
+      <div id="top-row">
+          <img id="img" src="billeder/vineksperten.png" id="logo">    <!-- billede -->
+          <input class="top-row-btn" id="search-box" type="text" id="search" name="search" value="Søg"><!-- Søgefeldt -->
+          <button class="top-row-btn">Søg</button>
+          <button class="top-row-btn">Indkøbskurv</button><!-- Indkøbskurv -->
+      </div>
+      <hr>
+      <div id="sec-top-row">
+          <!-- Menu -->
+          <div class="menu-items">
+              <p id="rodvin">Rødvin</p>
+          </div>
+          <div class="menu-items">
+              <p id="hvidvin-menu">Hvidvin</p>
+          </div>
+          <div class="menu-items">
+              <p id="Rosevin-menu">Rosévin</p>
+
+          </div>
+          <div class="menu-items">
+              <p id="spiritus-menu">Spiritus</p>
+          </div>
+          <div class="menu-items">
+              <p id="delikatesser-menu">Delikatesser</p>
+          </div>
+      </div>
+  </header>
     <div id="product-wrapper">
         <div id="produkt-billede">
             <img src="<?php echo $product['image']; ?>">
@@ -45,7 +48,7 @@ $product = getJson($_GET['cat'])[$_GET['pid']];
                 <div id="produktnavn"><?php echo $product['title']; ?></div>
                 <div id="produkt-type-cat"><?php echo $product['country']; ?></div>
             </div>
-            <div id="produkt-pris"><?php echo $product['price']; ?></div> 
+            <div id="produkt-pris"><?php echo $product['price']; ?></div>
         </div>
         <br>
         <div id="product-details">
