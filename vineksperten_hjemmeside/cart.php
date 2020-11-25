9fb45ff913657cc766912db895f0bb61daf1ae8a
jhstig <73479559+jhstig@users.noreply.github.com>
2020-11-25 21:48:26 +0100

    lidt design til cart

--- vineksperten_hjemmeside/cart.php
+++ vineksperten_hjemmeside/cart.php
@@ -21,16 +21,23 @@ $totalPrice = 0;
 
     <div id="checkout-wrapper">
         <div id="checkout-left-wrapper">
-            <div id="checkout-lorem"></div>
-            <div id="checkout-commercial"></div>
-            <div id="checkout-purchase"></div>
+            <div id="checkout-lorem">
+                
+                Kortnummer: <input type="text">
+                <br>
+                Navn: <input type="text">
+                <br>
+                CCT: <input type="text">
+            </div>
+            <div id="checkout-address"></div>
+            <div id="checkout-purchase"><button>Køb</button></div>
         </div>
         <div id="checkout-products-wrapper">
             <div id="checkout-products-picture">
                 <!-- her skal være et loop -->
                 <?php for($i = 1; $i < count($users[$userid]['cart']); $i++) {?>
                 <div class="checkout-pictures">
-                    <img class="checkout-pictures-raw" src="<?php echo $users[$userid]['cart']["item" . $i]['img']; ?>" alt="">
+                    <img src="<?php echo $users[$userid]['cart']["item" . $i]['img']; ?>" alt="">
                 </div>
                 <?php } ?>
                 <!--            -->
@@ -41,7 +48,7 @@ $totalPrice = 0;
                 <?php for($i = 1; $i < count($users[$userid]['cart']); $i++) {?>
                 <div class="checkout-descriptions">
                     <p id="product-title-p"><?php echo $users[$userid]['cart']["item" . $i]['title']; ?></p>
-                    <p id="product-amount"><?php echo $users[$userid]['cart']["item" . $i]['amount']; ?></p>
+                    <p id="product-amount">Stk: <?php echo $users[$userid]['cart']["item" . $i]['amount']; ?></p>
                 </div>
                 <?php } ?>
                 <!--            -->
@@ -50,10 +57,9 @@ $totalPrice = 0;
         <div id="checkout-sub-total">
             <?php for($i = 1; $i < count($users[$userid]['cart']); $i++) {?>
                 <div class="checkout-sub-calculation">
-                    <p>Vare: <?php echo $users[$userid]['cart']["item" . $i]['title']; ?></p>
-                    <p>Antal stk.: <?php echo $users[$userid]['cart']["item" . $i]['amount']; ?></p>
-                    <p>Pris pr. stk.: <?php echo $users[$userid]['cart']["item" . $i]['price']; ?></p>
-                    <p>Samlet: <?php echo $users[$userid]['cart']["item" . $i]['amount'] * str_replace(",",".",$users[$userid]['cart']["item" . $i]['price']); ?></p>
+                    <p><?php echo $users[$userid]['cart']["item" . $i]['title']; ?></p>
+                    <p>Antal stk. <?php echo $users[$userid]['cart']["item" . $i]['amount']; ?> til prisen <?php echo $users[$userid]['cart']["item" . $i]['price']; ?></p>
+                    <p>Samlet <?php echo $users[$userid]['cart']["item" . $i]['amount'] * str_replace(",",".",$users[$userid]['cart']["item" . $i]['price']); ?> DKK</p>
                     <?php 
                     $tempPrice = 0;
                     $tempPrice = $users[$userid]['cart']["item" . $i]['amount'] * str_replace(",",".",$users[$userid]['cart']["item" . $i]['price']);
