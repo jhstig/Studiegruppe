6477afdedfdb5258d106a84f07d5c8f794b50272
jhstig <73479559+jhstig@users.noreply.github.com>
2020-11-25 16:08:51 +0100

    lidt småt

--- vineksperten_hjemmeside/cart.php
+++ vineksperten_hjemmeside/cart.php
@@ -12,6 +12,25 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <div id="checkout-wrapper">
+        <div id="checkout-left-wrapper">
+            <div id="checkout-lorem"></div>
+            <div id="checkout-commercial"></div>
+            <div id="checkout-purchase"></div>
+        </div>
+        <div id="checkout-products-wrapper">
+            <div id="checkout-products-picture">
+                <div class="checkout-pictures"></div>
+                <div class="checkout-pictures"></div>
+            </div>
+            <div id="checkout-products-description">
+                <div class="checkout-descriptions"></div>
+                <div class="checkout-descriptions"></div>
+            </div>
+        </div>
+        <div id="checkout-sub-total">
+
+        </div>
+    </div>
 </body>
 </html>
